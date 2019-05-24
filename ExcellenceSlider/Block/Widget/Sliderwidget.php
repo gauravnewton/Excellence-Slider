2f9e0fec9eefa7fda7946dca4d8744f1297c2027
singharti <artisingh3106@gmail.com>
2019-05-24 18:28:27 +0530

    Created Slider By Widget

---
+++ ExcellenceSlider/Block/Widget/Sliderwidget.php
@@ -0,0 +1,8 @@
+<?php
+namespace Excellence\ExcellenceSlider\Block\Widget;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+class Sliderwidget extends Template implements BlockInterface
+{
+    protected $_template = "widget/sliderwidget.phtml";
+}
---
+++ ExcellenceSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+   paths: {
+       'owlcarousel': "Excellence_ExcellenceSlider/js/owl.carousel"
+   },
+   
+   shim: {
+       'owlcarousel': {
+           deps: ['jquery']
+       }
+   }
+};
